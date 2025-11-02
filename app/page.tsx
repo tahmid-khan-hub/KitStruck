334a5c0042137c4395b46deca885d8d8e0e623fd
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-02 22:22:31 +0600

    Imported Trending, TopRated and Feedback section in the root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Feedback from "./components/Feedback/Feedback";
+import TopRated from "./components/TopRated/TopRated";
+import Trending from "./components/Trending/Trending";
 
 export default function Home() {
   return (
-    <div>
-      home
+    <div className="text-black max-w-[1350px] mx-auto px-4 md:px-3">
+      <Trending></Trending>
+      <TopRated></TopRated>
+      <Feedback></Feedback>
     </div>
   );
 }

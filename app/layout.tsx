3695d67c0113481bcf21293ba84e7bee6b938bcf
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-02 22:05:06 +0600

    Added Inter and Passero_one font in the layout and global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Passero_One } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const passero = Passero_One({
+  variable: "--font-passero",
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${passero.variable} antialiased inter`}
       >
         {children}
       </body>

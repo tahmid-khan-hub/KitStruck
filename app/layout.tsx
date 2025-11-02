8657d6786e3045741001d273151a7a9173a77c31
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-02 23:14:12 +0600

    Added data theme in the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body
         className={`${inter.variable} ${passero.variable} antialiased inter`}
       >

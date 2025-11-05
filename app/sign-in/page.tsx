f718d21c88296b7d19cff68d419689f86d95de06
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-06 00:06:34 +0600

    Added lottie and updated the structure of sign-in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+import Lottie from "react-lottie-player";
+import SignInLottie from "@/public/sign-in.json"
 const LoginPage = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center px-4">
-      <div className="card w-full max-w-md bg-base-200 shadow-xl p-8 space-y-6">
+    <div className="min-h-screen"><div className="flex flex-col md:flex-row max-w-[1350px] mx-auto">
+      {/* Lottie */}
+      <div className="min-h-screen hidden md:flex w-1/2 items-center justify-center bg-base-200">
+        <Lottie
+          play
+          loop
+          animationData={SignInLottie} 
+          className="w-3/4 max-w-md"
+        />
+      </div>
+
+      <div className="flex w-full md:w-1/2 items-center justify-center p-8"><div className="card w-full max-w-md bg-base-200 shadow-xl p-8 space-y-6">
         {/* Title */}
         <h2 className="text-3xl font-bold text-center">Welcome Back</h2>
 
@@ -43,8 +56,8 @@ const LoginPage = () => {
           <svg aria-label="Google logo" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
           Continue with Google
         </button>
-      </div>
-    </div>
+      </div></div>
+    </div></div>
   );
 };
 

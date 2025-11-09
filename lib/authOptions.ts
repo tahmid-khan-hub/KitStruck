9d9f22fa425c56faa9372b89508e7bdc8f3f157f
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-09 19:41:40 +0600

    Added callbacks in the authOptions to store user in the database for Google signIn

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,4 +1,15 @@
 import GoogleProvider from "next-auth/providers/google";
+import pool from "./mysql";
+import { RowDataPacket } from "mysql2";
+import { User } from "next-auth";
+
+interface DBUser {
+  id: number;
+  name: string;
+  email: string;
+  photoURL: string | null;
+  role: string;
+}
 
 export const authOptions = {
   providers: [
@@ -8,7 +19,33 @@ export const authOptions = {
     }),
   ],
   pages: {
-    signIn: '/sign-in', // Custom login page
+    signIn: "/sign-in", // Custom login page
   },
   secret: process.env.NEXTAUTH_SECRET,
-}
+
+  callbacks: {
+    async signIn({ user }: { user: User }) {
+      try {
+        const dbConnect = await pool.getConnection();
+
+        const [rows] = await dbConnect.query<DBUser[] & RowDataPacket[]>(
+          "SELECT * FROM users WHERE email = ?",
+          [user?.email]
+        );
+
+        if (rows.length === 0) {
+          await dbConnect.query(
+            "INSERT INTO users (name, email, photoURL, role) VALUES (?, ?, ?, ?)",
+            [user.name, user.email, user.image, "user"]
+          );
+        }
+
+        dbConnect.release();
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+};

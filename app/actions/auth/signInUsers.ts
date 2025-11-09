562dd8607e38617eef8fa3d87f951283106cb715
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-09 19:53:46 +0600

    Added handleSubmit function in the SignInForm, passed the email and password to the newly added signInUsers

---
+++ app/actions/auth/signInUsers.ts
@@ -0,0 +1,6 @@
+export const signInUsers = async(payload: {
+    email: string;
+    password: string;
+}) =>{
+
+}
--- app/sign-in/components/SignInForm.tsx
+++ app/sign-in/components/SignInForm.tsx
@@ -1,15 +1,38 @@
+import { signInUsers } from "@/app/actions/auth/signInUsers";
+import { FormEvent } from "react";
+
 const SignInForm = () => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    const res = await signInUsers({email, password});
+
+    if (res) {
+      alert("Sign in successful!");
+      form.reset();
+    } else {
+      alert("Something went wrong. Sign in is not successful");
+    }
+  };
   return (
     <div>
-      <form className="space-y-4">
+      <form onSubmit={handleSubmit} className="space-y-4">
         <input
           type="email"
+          name="email"
           placeholder="Enter your email"
           className="input input-bordered w-full"
           required
         />
         <input
           type="password"
+          name="password"
           placeholder="Enter your password"
           className="input input-bordered w-full"
           required

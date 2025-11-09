101d878490b5beb63e81740afdc0da88fed65bcc
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-09 23:29:35 +0600

    Set up the middleware

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export const middleware = async (req: NextRequest) => {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  // token exists, allow access
+  if (token) return NextResponse.next();
+
+  // Otherwise redirect -> sign in page
+  return NextResponse.redirect(new URL("/sign-in", req.url));
+};
+
+// Paths that require authentication
+export const config = {
+  matcher: ["/my-bookings/:path*", "/dashboard"],
+};

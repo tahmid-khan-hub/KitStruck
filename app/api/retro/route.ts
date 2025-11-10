1ce0c066d2761b7499d831c9334c645ac0e3a251
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-10 21:40:28 +0600

    Added the retro route in the api

---
+++ app/api/retro/route.ts
@@ -0,0 +1,21 @@
+import pool from "@/lib/mysql";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const dbConnect = await pool.getConnection();
+
+    const [rows] = await dbConnect.query(
+      `SELECT * FROM jerseys WHERE category = "retro" ORDER BY sells_quantity DESC LIMIT 6`
+    );
+
+    dbConnect.release();
+
+    return NextResponse.json(rows);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Failed to fetch retro collections" }, { status: 500 }
+    );
+  }
+}

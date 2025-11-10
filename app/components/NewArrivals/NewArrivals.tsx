f8fa02be319db074b63dc3adf464648fb0ee106c
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-10 21:51:30 +0600

    Updated the newArrivals section cards design and structure

--- app/components/NewArrivals/NewArrivals.tsx
+++ app/components/NewArrivals/NewArrivals.tsx
@@ -2,8 +2,7 @@
 import { Jersey } from "@/types/jersey";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { FaShoppingCart } from "react-icons/fa";
-import { MdOutlinePayment } from "react-icons/md";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const NewArrivals = () => {
   const [data, setData] = useState<Jersey[]>([]);
@@ -15,37 +14,38 @@ const NewArrivals = () => {
       .catch((err) => console.error("Fetch error:", err));
   }, []);
   return (
-    <div className="my-24">
-      <h2 className="text-3xl text-center font-bold mb-8">New Arrivals</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {data.map((jersey) => (
-          <div
-            key={jersey.jersey_id}
-            className="p-3 bg-white rounded-lg flex flex-col justify-between hover:shadow-md transition"
-          >
-            <Image
-              src={jersey.image_url}
-              alt={jersey.name}
-              width={200}
-              height={200}
-              className="object-cover rounded-md w-full h-[350px]"
-            />
-            <div className="mt-4">
-              <h3 className="font-semibold text-lg">{jersey.name}</h3>
-              <p className="text-gray-600">${jersey.price}</p>
+    <div>
+      <div className="my-24">
+        <h2 className="text-3xl text-center font-bold mb-8">
+          New Arrivals
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {data.map((jersey) => (
+            <div
+              key={jersey.jersey_id}
+              className="p-3 bg-white rounded-lg flex flex-col justify-between hover:shadow-md transition"
+            >
+              <Image
+                src={jersey.image_url}
+                alt={jersey.name}
+                width={200}
+                height={200}
+                className="object-cover rounded-md w-full h-[350px]"
+              />
+              <div className="mt-4 flex flex-col">
+                <h3 className="font-semibold text-lg mb-3">{jersey.name}</h3>
+                <div className="mt-8 flex items-center justify-between">
+                  <p className="text-gray-800 font-semibold text-lg">
+                    ${jersey.price}
+                  </p>
+                  <button className="flex items-center justify-center text-blue-500 p-2 rounded-md transition">
+                    <FaArrowRightLong />
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="mt-8 flex justify-between items-center gap-3">
-              <button className="flex items-center justify-center gap-2 px-5 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
-                <MdOutlinePayment />
-                <span>Buy</span>
-              </button>
-              <button className="flex items-center justify-center gap-2 px-5 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300 transition">
-                <FaShoppingCart />
-                <span>Add to Cart</span>
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );

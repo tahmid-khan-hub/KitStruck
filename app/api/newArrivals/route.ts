1f65f81032ac4702ef4b0f79bb0778ea83ec2fc7
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-10 17:32:16 +0600

    Added newArrivals api route

---
+++ app/api/newArrivals/route.ts
@@ -0,0 +1,21 @@
+import pool from "@/lib/mysql";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const dbConnect = await pool.getConnection();
+
+    const [rows] = await dbConnect.query(
+      "SELECT * FROM jerseys ORDER BY created_at DESC LIMIT 3"
+    );
+
+    dbConnect.release();
+
+    return NextResponse.json(rows);
+  } catch (error) {
+    console.error("Error fetching new arrivals:", error);
+    return NextResponse.json(
+      { message: "Failed to fetch new arrivals" }, { status: 500 }
+    );
+  }
+}

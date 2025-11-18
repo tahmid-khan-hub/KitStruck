ac65a5a1189037b09dd7baf6aaab573494d2d9da
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-18 20:48:57 +0600

    Removed the unused links from navbar and added a about page

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
-import Image from "next/image";
 import { signOut, useSession } from "next-auth/react";
 import Menu from "@/app/hooks/Menu";
 import Logo from "@/app/hooks/Logo";

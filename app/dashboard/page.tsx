6adf49cd7b4e04b6748de498bcd195b24ec2d459
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-24 17:25:21 +0600

    Updated the dashboard page, redirecting user to specific page according to specific role

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,15 @@
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
 export default async function DashBoardPage() {
+    const session = await getServerSession(authOptions);
+
+    if(!session) redirect("/sign-in");
+
+    const role = session.user.role;
+
+    if(role === "admin") redirect("/dashboard/admin");
+    else redirect("/dashboard/user");
     
 }

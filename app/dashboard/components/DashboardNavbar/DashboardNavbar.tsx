e3c15e4dc14eb83fb547127aa2913e05e97dcf12
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-11-25 18:36:02 +0600

    Added a new components called DashboardNavbar and imported it inside the dashboard layout, also added Google image domain inside the next config

---
+++ app/dashboard/components/DashboardNavbar/DashboardNavbar.tsx
@@ -0,0 +1,53 @@
+"use client"
+import Logo from "@/app/hooks/Logo";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+export default function DashboardNavbar() {
+  const { data: session } = useSession(); 
+  const user = session?.user;
+  return (
+    <div className="navbar sticky top-0 z-50 bg-base-100 w-full lg:hidden">
+      {/* menu */}
+      <div className="flex-none">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="open sidebar"
+          className="btn btn-square btn-ghost"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-6 w-6 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </label>
+      </div>
+
+      {/* user display name & photo beside logo */}
+      <div className="mx-2 flex-1 mb-1 lg:hidden">
+        <div className="flex items-center">
+          <Logo></Logo>
+          {user && (
+            <div className="flex items-center ml-auto">
+              <Image
+                src={user?.image ?? "/default-user.png"}
+                alt="User"
+                width={22}
+                height={22}
+                className="rounded-full object-cover ml-2"
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,17 @@
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+import DashboardNavbar from "./components/DashboardNavbar/DashboardNavbar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div>
-        {children}
+    <div className="drawer lg:drawer-open max-w-[1500px] mx-auto border border-gray-50">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+
+      {/* navbar for small screen */}
+      <DashboardNavbar></DashboardNavbar>
+      {children}
     </div>
   );
 }

e04b1614d8e610f831abbb4b5f42c6ec666b9c7f
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-12-01 18:51:44 +0600

    Added a checkoutform and updated the payment page

---
+++ app/payment/CheckoutForm.tsx
@@ -0,0 +1,3 @@
+export default function CheckoutForm({amount, jerseyId}:{amount: number; jerseyId: number;}) {
+    
+}
--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import CheckoutForm from "./CheckoutForm";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
@@ -11,5 +13,23 @@ export default function PaymentPage() {
     const JerseyId = Number(params.get("jersey_id")); 
     const [clientSecret, setClientSecret] = useState("");
     
+    useEffect(()=>{
+        fetch("/api/payment-intent", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                amount, JerseyId,
+            }),
+        })
+        .then((res) => res.json())
+        .then((data) => setClientSecret(data.clientSecret));
+    },[amount, JerseyId]);
 
+    if (!clientSecret) return <p>Loading payment...</p>;
+
+    return(
+        <Elements stripe={stripePromise} options={{clientSecret}}>
+            <CheckoutForm amount={amount} jerseyId={JerseyId}></CheckoutForm>
+        </Elements>
+    )
 }

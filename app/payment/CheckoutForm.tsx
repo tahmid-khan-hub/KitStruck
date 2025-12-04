2e6e5d7ac3dae6cb87dd9dcc61aacff4cd1f6bdc
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-12-04 16:15:24 +0600

    Updated the payment form style and also added lottie in the payment page

--- app/payment/CheckoutForm.tsx
+++ app/payment/CheckoutForm.tsx
@@ -2,6 +2,8 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useState } from "react";
 import UseSweetAlert from "../hooks/UseSweetAlert";
+import Lottie from "react-lottie-player";
+import PaymentLottie from "@/public/Payment.json"
 
 export default function CheckoutForm({
   amount,
@@ -12,23 +14,23 @@ export default function CheckoutForm({
   jerseyId: number;
   clientSecret: string;
 }) {
-  const {errorToast, successToast} = UseSweetAlert();
+  const { errorToast, successToast } = UseSweetAlert();
   const stripe = useStripe();
   const elements = useElements();
   const [loading, setLoading] = useState(false);
 
-  const handlePayment = async(e: React.FormEvent) => {
+  const handlePayment = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    if(!stripe || !elements) {
-        setLoading(false);
-        return;
+    if (!stripe || !elements) {
+      setLoading(false);
+      return;
     }
 
     const cardElement = elements.getElement(CardElement);
-    if(!cardElement){
-        setLoading(false);
-        return;
+    if (!cardElement) {
+      setLoading(false);
+      return;
     }
 
     const { error, paymentIntent } = await stripe.confirmCardPayment(
@@ -58,18 +60,25 @@ export default function CheckoutForm({
       });
     }
     setLoading(false);
-  }
+  };
 
   return (
     <div>
       <form onSubmit={handlePayment}>
-        <CardElement className="p-4 border rounded-lg" />
-        <button
-          disabled={!stripe || loading}
-          className="bg-blue-600 text-white px-6 py-3 rounded-lg w-full"
-        >
-          {loading ? "Processing..." : `Pay $${amount}`}
-        </button>
+        <div className="min-h-screen">
+          <h2 className="text-center text-3xl font-bold mt-11">Payment Form</h2>
+          <div className="mb-5"><Lottie play loop animationData={PaymentLottie} className="w-[250px] max-w-md mx-auto"
+          /></div>
+          <div className="px-2"><div className="max-w-[550px] mx-auto bg-white border border-gray-300 shadow-md hover:shadow-lg rounded-lg p-4 ">
+            <CardElement className="p-4 mt-2.5 border rounded-xl" />
+            <button
+              disabled={!stripe || loading}
+              className="btns mt-7 mb-3 px-6 py-3 w-full"
+            >
+              {loading ? "Processing..." : `Pay $${amount}`}
+            </button>
+          </div></div>
+        </div>
       </form>
     </div>
   );

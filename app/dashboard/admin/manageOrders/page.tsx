ea1c95889025a60773efb3ba8003cb28db9707d0
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-01-10 22:19:09 +0600

    Updated the manageOrders page by adding axios and tan stack

--- app/dashboard/admin/manageOrders/page.tsx
+++ app/dashboard/admin/manageOrders/page.tsx
@@ -1,47 +1,36 @@
 "use client";
 import { orders } from "@/types/orders";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import OrdersSkeleton from "../../components/OrdersTable/OrdersSkeleton";
 import Lottie from "react-lottie-player";
 import noOrders from "@/public/nodata.json"
 import OrdersTable from "../../components/OrdersTable/OrdersTable";
 import DashboardTablesPagination from "../../components/DashboardTablesPagination/DashboardTablesPagination";
+import useAxiosSecure from "@/app/hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const LIMIT = 5;
 
 export default function ManageOrders() {
   const [page, setPage] = useState(1);
-  const [orders, setOrders] = useState<orders[]>([]);
-  const [totalPages, setTotalPages] = useState(1);
-  const [loading, setLoading] = useState(true);
+  const axiosSecure = useAxiosSecure();
 
-  useEffect(() => {
-    let ignore = false;
+  const{data, isLoading} = useQuery({
+    queryKey: ["manage-orders", page],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/api/admin/manage-orders?page=${page}&limit=${LIMIT}`);
 
-    async function fetchOrders() {
-      setLoading(true);
-
-      const res = await fetch(`/api/admin/manage-orders?page=${page}&limit=${LIMIT}`);
-      const data = await res.json();
-
-      if (!ignore) {
-        setOrders(data.data ?? []);
-        setTotalPages(data.totalPages ?? 1);
-        setLoading(false);
-      }
+      return res.data;
     }
+  })
 
-    fetchOrders();
-
-    return () => {
-      ignore = true;
-    };
-  }, [page]);
+  const orders: orders[] = data?.data ?? [];
+  const totalPages = data?.totalPages ?? 1;
 
   return (
     <div className="w-full p-6">
       <h1 className="text-3xl text-center font-bold mb-11 mt-5">Manage Orders</h1>
-      {loading ? (
+      {isLoading ? (
         <OrdersSkeleton rows={LIMIT} />
       ) : orders.length === 0 ? (
         <div className="flex flex-col items-center justify-center py-10">

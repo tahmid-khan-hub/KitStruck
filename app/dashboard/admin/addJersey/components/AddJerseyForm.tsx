ad9ab5a3306e1a36e6d2a82d5dda553694961249
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-01-10 16:20:48 +0600

    Updated the addjersey form

--- app/dashboard/admin/addJersey/components/AddJerseyForm.tsx
+++ app/dashboard/admin/addJersey/components/AddJerseyForm.tsx
@@ -4,11 +4,29 @@ import { FormEvent, useState } from "react";
 import UseSweetAlert from "@/app/hooks/UseSweetAlert";
 import FormFields from "./FormFields";
 import { motion } from "framer-motion";
+import useAxiosSecure from "@/app/hooks/useAxiosSecure";
+import { useMutation } from "@tanstack/react-query";
 
 const AddJerseyForm = () => {
 
   const { successToast, errorToast } = UseSweetAlert();
   const [category, setCategory] = useState("");
+  const axiosSecure = useAxiosSecure();
+
+  const{mutate, isPending} = useMutation({
+    mutationFn: async({jerseyData}:{jerseyData: object}) => {
+      const res = await axiosSecure.post(`/api/admin/add-jersey`, {
+        jerseyData
+      });
+      return res.data;
+    },
+    onSuccess: () => {
+      successToast("New Jersey has been added successfully!");
+    },
+    onError: () => {
+      errorToast("Failed to add new jersey. Something went wrong!");
+    }
+  })
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -25,26 +43,16 @@ const AddJerseyForm = () => {
       stock: formdata.get("stock"),
     };
 
-    const res = await fetch("/api/admin/add-jersey", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ jerseyData }),
-    });
-
-    if (res.ok) {
-      successToast("New Jersey has been added successfully!");
-      form.reset();
-      setCategory("");
-    } else {
-      errorToast("Failed to add new jersey. Something went wrong!");
-    }
+    mutate({jerseyData});
+    form.reset();
+    
   };
   return (
     <div className="px-20">
       <motion.form initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} 
         onSubmit={handleSubmit} className="space-y-4">
           <FormFields category={category} setCategory={setCategory} />
-        <button className="w-full btns my-7">Add Jersey</button>
+        <button disabled={isPending} type="submit" className="w-full btns my-7">{isPending ? "Adding..." : "Add Jersey"}</button>
       </motion.form>
     </div>
   );

9ebb754d8705581858526e1fe6e798a13a6b34ed
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-01-22 23:53:42 +0600

    Updated the paymentPage using suspense and added PaymentPageClient component

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,35 +1,11 @@
-"use client";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import CheckoutForm from "./CheckoutForm";
+import { Suspense } from "react";
 import PaymentSkeleton from "../SkeletonLoading/PaymentSkeleton";
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+import PaymentPageClient from "./PaymentPageClient";
 
 export default function PaymentPage() {
-    const params = useSearchParams();
-    const order_id_params = params.get("order_id");
-    const order_id = Number(order_id_params)
-    const [clientSecret, setClientSecret] = useState("");
-    
-    useEffect(()=>{
-        if(!order_id) return;
-        fetch("/api/payment/payment-intent", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ order_id: Number(order_id) }),
-        })
-        .then((res) => res.json())
-        .then((data) => setClientSecret(data.clientSecret));
-    },[order_id]);
-
-    if (!clientSecret) return <PaymentSkeleton />;
-
     return(
-        <Elements stripe={stripePromise} options={{clientSecret}}>
-            <CheckoutForm order_id={order_id} clientSecret={clientSecret}></CheckoutForm>
-        </Elements>
+        <Suspense fallback={<PaymentSkeleton />}>
+            <PaymentPageClient />
+        </Suspense>
     )
 }
